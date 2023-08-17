dc9d055fd94a214611261c63a4c44081f36f4172
playsenseme <55314837+mx010xm@users.noreply.github.com>
2023-08-17 23:56:11 +0300

    Initial commit

---
+++ src/test/java/com/sense/productcatalog/ProductcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sense.productcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

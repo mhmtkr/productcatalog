1d97ed59fe1d61503b6c86d8e9e7c052f6b2ae42
playsenseme <55314837+mx010xm@users.noreply.github.com>
2023-08-21 23:31:14 +0300

    -Added some ProductRepository methods

--- src/main/java/com/sense/productcatalog/repository/ProductRepository.java
+++ src/main/java/com/sense/productcatalog/repository/ProductRepository.java
@@ -1,8 +1,16 @@
 package com.sense.productcatalog.repository;
 
 import com.sense.productcatalog.entity.Product;
+
 import org.springframework.data.couchbase.repository.CouchbaseRepository;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface ProductRepository extends CouchbaseRepository<Product, String> {
+
+    List<Product> findByName(String name);
+
+    List<Product> findByPriceGreaterThan(BigDecimal price);
 
 }

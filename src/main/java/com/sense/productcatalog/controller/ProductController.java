83adc058fb1442d7186e98e8dd115df225aef947
playsenseme <55314837+mx010xm@users.noreply.github.com>
2023-08-24 23:22:31 +0300

    -Added API endpoints for updating and deleting a product

--- src/main/java/com/sense/productcatalog/controller/ProductController.java
+++ src/main/java/com/sense/productcatalog/controller/ProductController.java
@@ -31,4 +31,31 @@ public class ProductController {
             return ResponseEntity.notFound().build();
         }
     }
+
+    // Update a product by its ID
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProductById(@PathVariable("id") String id, @RequestBody Product updatedProduct) {
+        Product product = productService.getProductById(id);
+        if (product != null) {
+            product.setName(updatedProduct.getName());
+            product.setDescription(updatedProduct.getDescription());
+            product.setPrice(updatedProduct.getPrice());
+            product.setAvailability(updatedProduct.getAvailability());
+            Product updatedProduct = productService.updateProduct(product);
+            return ResponseEntity.ok(updatedProduct);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // Delete a product by its ID
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProductById(@PathVariable("id") String id) {
+        boolean deleted = productService.deleteProductById(id);
+        if (deleted) {
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
